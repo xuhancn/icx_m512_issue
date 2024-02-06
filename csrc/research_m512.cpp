e765dfce0967f75a60656f0aa43af850ca668bb8
Xu Han <xu.han@outlook.com>
2024-02-06 21:47:04 +0800

    add files.

---
+++ csrc/research_m512.cpp
@@ -0,0 +1,20 @@
+#include <immintrin.h>
+
+int main()
+{
+    __m512i vals;
+    #ifdef _MSC_VER
+    __m128i int_val0 = _mm_set_epi64x(vals.m512i_u64[1], vals.m512i_u64[0]);
+    __m128i int_val1 = _mm_set_epi64x(vals.m512i_u64[3], vals.m512i_u64[2]);
+    __m128i int_val2 = _mm_set_epi64x(vals.m512i_u64[5], vals.m512i_u64[4]);
+    __m128i int_val3 = _mm_set_epi64x(vals.m512i_u64[7], vals.m512i_u64[6]);
+    #else
+    __m128i int_val0 = _mm_set_epi64x(vals[1], vals[0]);
+    __m128i int_val1 = _mm_set_epi64x(vals[3], vals[2]);
+    __m128i int_val2 = _mm_set_epi64x(vals[5], vals[4]);
+    __m128i int_val3 = _mm_set_epi64x(vals[7], vals[6]);
+    #endif
+
+
+    return 0;
+}
